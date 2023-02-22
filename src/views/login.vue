da33b5836db8a5ed9ad8d973c1b9fff5f82d6924
刘婉 <lw@eteng.cn>
2023-02-22 16:29:49 +0800

    登录页面

--- src/views/login.vue
+++ src/views/login.vue
@@ -4,15 +4,16 @@
     <div class="title">欧普康视<br />售后工单系统</div>
     <div>
       <van-form @submit="onSubmit">
-        <van-field v-model="tel" :formatter="formatter" type="tel" label="手机号" placeholder="请输入手机号"
-          error-message="手机号格式错误" />
-        <van-field v-model="yzm" center clearable label="验证码" placeholder="请输入验证码">
+        <van-field v-model="param.tel" label="手机号" :rules="userFormRules.tel" placeholder="请输入手机号" type="number"
+          maxlength="11"/>
+        <van-field v-model="param.yzm" center clearable label="验证码" :rules="userFormRules.yzm" type="number"
+          maxlength="6" placeholder="请输入验证码">
           <template #button>
-            <van-button size="small" type="info">发送验证码</van-button>
+            <van-button native-type="button" size="small" type="info">发送验证码</van-button>
           </template>
         </van-field>
         <div class="mgt48">
-          <van-button round block type="info" native-type="submit">提交l </van-button>
+          <van-button round block type="info" native-type="submit">提交 </van-button>
         </div>
 
       </van-form>
@@ -32,8 +33,26 @@
     props: {},
     data() {
       return {
-        tel: '',
-        yzm: ''
+        param: {
+          tel: '',
+          yzm: ''
+        },
+        userFormRules: {
+          tel: [{
+            required: true,
+            message: '手机号不能为空'
+          }, {
+            pattern: /^1[3,4,5,6,7,8][0-9]{9}$/,
+            message: '手机号格式错误'
+          }],
+          yzm: [{
+            required: true,
+            message: '验证码不能为空'
+          }, {
+            pattern: /^d{6}$/,
+            message: '验证码格式错误'
+          }]
+        },
       }
     },
     computed: {},
@@ -41,13 +60,34 @@
     created() {},
     mounted() {},
     methods: {
-      onSubmit() {
-       
+      async onSubmit(values) {
+        console.log('submit', values)
+         // 1. 获取表单数据
+      // const user = this.user
+
+      // // TODO: 2. 表单验证
+
+      // // 3. 提交表单请求登录
+      // this.$toast.loading({
+      //   message: '登录中...',
+      //   forbidClick: true, // 禁用背景点击
+      //   duration: 0 // 持续时间，默认 2000，0 表示持续展示不关闭
+      // })
+      // try {
+      //   const res = await login(user)
+      //   console.log('登录成功', res)
+      //   this.$toast.success('登录成功')
+      // } catch (err) {
+      //   if (err.response.status === 400) {
+      //     this.$toast.fail('手机号或验证码错误')
+      //   } else {
+      //     this.$toast.fail('登录失败，请稍后重试')
+      //   }
+      // }
+
+      // 4. 根据请求响应结果处理后续操作
+
       },
-       formatter(value) {
-          // 过滤输入的数字
-          return value.replace(/\d/g, '');
-        },
     }
   }
 </script>
